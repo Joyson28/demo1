e7388e86e495186f075058bac6c7d8a9915f35c9
sakshi <poojary28102001@gmail.com>
2024-02-03 15:17:10 +0530

    Implement new feature

---
+++ sss.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const hello = () => {
+  return (
+    <View>
+      <Text>ppppppppppppppppppppppppppppppppppppppppp</Text>
+      
+
+
+
+    </View>
+  )
+}
+
+export default hello
+
+const styles = StyleSheet.create({})
